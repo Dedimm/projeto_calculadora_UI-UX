ab369711d79af65d26ed6c91b9cbceff89625478
Dedim <marcio.spxtz04@outlook.com>
2026-05-14 16:16:10 -0300

    REFATORACAO FEITA

--- src/projeto_calculadora/Calculadora.java
+++ src/projeto_calculadora/Calculadora.java
@@ -1,36 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package projeto_calculadora;
 
-/**
- *
- * @author marci
- */
 public class Calculadora {
-    public int r=0;
 
-    
-    public int calc(int n1, int n2, int operacao){
-        if(operacao == 1){
-            r = n1 + n2;
+    public int calc(int n1, int n2, int operacao) {
+        switch (operacao) {
+            case 1: return n1 + n2;
+            case 2: return n1 - n2;
+            case 3: return n1 * n2;
+            case 4:
+                if (n2 == 0) {
+                    System.out.println("Erro: nao pode dividir por 0");
+                    return 0;
+                }
+                return n1 / n2;
         }
-        else if(operacao == 2){
-            r = n1 - n2;
-        }
-        else if(operacao == 3){
-            r = n1 * n2;
-        }
-        else if(operacao == 4){
-            if(n2 != 0){
-                r = n1 / n2;
-            }else{
-                System.out.println("Erro, nao pode dividir por 0");
-                r = 0;
-            }
-        }
-   
-        return r;
+        return 0;
     }
 }
